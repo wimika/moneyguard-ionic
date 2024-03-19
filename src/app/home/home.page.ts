6f7aad3c124aecf40bc1f2cba7f53b0a21ae6ba9
Frank Akogun <frankyotana@yahoo.com>
2024-03-19 23:49:13 +0100

    breaking changes for plugin

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { HashAlgorithm } from 'Moneyguard/www/MoneyGuardTypes/HashAlgorithm';
+import { CredentialScanResult } from 'Moneyguard/www/MoneyGuardTypes/CredentialScanResult';
+import { DebitTransactionCheckResult } from 'Moneyguard/www/MoneyGuardTypes/DebitTransactionCheckResult';
+import { RiskStatus } from 'Moneyguard/www/MoneyGuardTypes/RiskStatus';
+import { SpecificRisk } from 'Moneyguard/www/MoneyGuardTypes/SpecificRisk';
+import { RiskResult } from 'Moneyguard/www/MoneyGuardTypes/RiskResult';
+
+// Declare cordova to avoid type errors
 declare var cordova: any;
 
 @Component({
@@ -7,13 +15,24 @@ declare var cordova: any;
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  constructor() {}
+  constructor() { }
 
+
+
+
+
+  
+
+  // Method to check plugin functionality
   checkPlugin() {
-    const wimika = cordova.plugins.Moneyguard;
-    console.log(wimika);
-    wimika.echo(
-      'testing',
+    // Access Moneyguard plugin from cordova
+    const { registerGuard } = cordova.plugins.Moneyguard;
+    const partnerBankId = "";
+    const sessionToken = "";
+
+    registerGuard(
+      partnerBankId,
+      sessionToken,
       (result: any) => {
         console.log(result);
       },
@@ -21,5 +40,70 @@ export class HomePage {
         console.log(error);
       }
     );
+
+
   }
+
+  checkCredential() {
+    const { checkCredential } = cordova.plugins.Moneyguard;
+
+    var credentialCheckReq = {
+      username: "example_user",
+      passwordStartingCharactersHash: "example_hash",
+      passwordFragmentLength: 10,
+      hashAlgorithm: HashAlgorithm.SHA256,
+      domain: "example.com"
+    };
+
+    checkCredential(
+      credentialCheckReq,
+      (result: any) => {
+        var res: CredentialScanResult = result;
+        console.log(res);
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    );
+  }
+
+
+  checkDebitTransaction() {
+    const { checkDebitTransaction } = cordova.plugins.Moneyguard;
+
+    var transactionReq =
+    {
+      sourceAccountNumber: "CHK-123456789", //Account to debit
+      amount: 100000,
+      memo: "Electricity Purchase",
+      destinationBank: "098", // Destination Bank Code
+      destinationAccountNumber: "KGD-987654321" //Destination Account Number
+    };
+
+    checkDebitTransaction(
+      transactionReq,
+      (result: any) => {
+        var res: DebitTransactionCheckResult = result;
+        console.log(res);
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    );
+  }
+
+  checkRiskProfile() {
+    const { getRiskProfile } = cordova.plugins.Moneyguard;
+
+    getRiskProfile(
+      (result: any) => {
+        var res: RiskResult = result;
+        console.log(result);
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    );
+  }
+
 }
